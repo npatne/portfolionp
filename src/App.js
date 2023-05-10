d7baf9636ac44d132a846df7de78a9f0987259fa
Nishad Patne <npatne@asu.edu>
2023-05-09 20:18:41 -0700

    Initial Remote Commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Making Initial setup.
+          Making Initial Remote commit.
         </p>
         <a
           className="App-link"
